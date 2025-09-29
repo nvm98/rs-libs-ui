eee39d6913649fedd0350a943accb4acb7ef26b5
nvm98 <manh98nv@gmail.com>
2025-09-29 11:33:19 +0700

    load and edit done

--- src/components/email-template/EmailEditorErrorState.tsx
+++ src/components/email-template/EmailEditorErrorState.tsx
@@ -11,15 +11,21 @@ export function EmailEditorErrorState({
 }: EmailEditorErrorStateProps) {
 
   return (
-    <BlockStack>
-        <Text as="h2" variant="heading2xl">Template not found</Text>
-        <Text as="p" variant="bodyLg">We couldn't load the template you're looking for. This might be due to a temporary issue or the template may no longer be available.</Text>
-        <Button
-          variant="primary"
-          onClick={() => handleTryAgain(templateName)}
-        >
-          Try again
-        </Button>
-    </BlockStack>
+    <Box paddingBlockStart={'800'}>
+      <BlockStack gap={'400'}>
+        <BlockStack gap={'200'}>
+          <Text as="h2" alignment="center" variant="headingLg">Template not found</Text>
+          <Text as="p" alignment="center" variant="bodyLg">We couldn't load the template you're looking for. This might be due to a temporary issue or the template may no longer be available.</Text>
+        </BlockStack>
+        <InlineStack align="center">
+          <Button
+            variant="primary"
+            onClick={() => handleTryAgain(templateName)}
+          >
+            Try again
+          </Button>
+        </InlineStack>
+      </BlockStack>
+    </Box>
   );
 }
